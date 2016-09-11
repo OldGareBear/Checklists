570bcdc086f4d87f149620026b96e0795d4d0065
OldGareBear <betheagary@gmail.com>
2016-09-11 12:47:08 -0400

    master: start to build out auth
    Signed-off-by: OldGareBear <betheagary@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,25 @@
 class User < ActiveRecord::Base
+
   validates :username, presence: true, length: {minimum: 3}
   validates :password_digest, :session_token, presence: true
 
   after_initialize :ensure_session_token
 
   has_and_belongs_to_many :checklists
+
+  def self.find_by_credentials(username, password)
+    user = self.find_by_username(username)
+    return user && valid_password?(password) ? user : nil
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
   def reset_token!
     self.session_token = SecureRandom.urlsafe_base64(16)
